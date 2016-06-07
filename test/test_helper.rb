ee0c749ef5cc48c6f3a40d136d00322999902708
daniel.vbranas <daniel.vbranas@udc.es>
2016-06-07 14:00:38 +0200

    Test framework created

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,51 @@ $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require 'thymeleaf'
 require 'minitest/autorun'
 require 'minitest/spec'
+
+class TestThymeleaf < Minitest::Test
+  def setup
+    @aux = Aux.new
+  end
+
+  def render(source, context = {})
+    Thymeleaf::Template.new(source, context).render
+  end
+
+  def assert_html(expected, source, context = {})
+    assert_equal expected, render(source, context)
+  end
+
+  def assert_syntax_error(message, source, context = {})
+    render(source, context)
+    raise 'Syntax error expected'
+  rescue Thymeleaf::Parser::SyntaxError => ex
+    assert_equal message, ex.message
+  end
+
+end
+
+class Aux
+
+  def some_short_text
+    'Hi, this is a short text'
+  end
+
+  def some_plain_text
+    <<-TXT
+    Lorem fistrum aliquip al ataquerl ex exercitation. Incididunt veniam a gramenawer elit aliqua tempor ex nisi.
+    Dolor diodeno eiusmod no puedor nostrud incididunt dioden.
+    TXT
+  end
+
+  def some_html_content
+    <<-HTML
+    <article>
+      <!-- A wild comment appeared! -->
+      <img src="path/to/image" alt="An image" class="main-image" />
+      <h2 class="">Some kind of title</h2>
+      <p class="description">A very <b>short</b> description</p>
+    </article>
+    HTML
+  end
+
+end
